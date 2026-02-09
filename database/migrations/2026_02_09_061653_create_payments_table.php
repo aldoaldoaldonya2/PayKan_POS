69b3ca9a85f1f73d74937e7479155fb4cd354a05
aldoaldoaldonya2 <aldonation77@gmail.com>
2026-02-09 13:23:56 +0700

    feat: migrations

---
+++ database/migrations/2026_02_09_061653_create_payments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('order_id');
+            $table->decimal('amount', 12, 2);
+            $table->string('status')->nullable();
+            $table->string('method');
+            $table->string('snap_token')->nullable();
+            $table->timestamp('payment_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
